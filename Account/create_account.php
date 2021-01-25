6975af3f691cbe524d97d0d145b56aa9179b8cab
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-01-25 19:31:22 +0100

    redirect fix

--- Account/create_account.php
+++ Account/create_account.php
@@ -19,7 +19,7 @@ $data = $result->fetch(PDO::FETCH_ASSOC);
 
 if (isset($data['email'])) { //Email already exists
     createMessage(2, 'This email is already in use');
-    redirect('../views/login.php');
+    redirect('views/login.php');
 } else {  //Email is available
     //Export new user to database
     $stmt = $db->prepare("INSERT INTO \"Users\" (email, password_hash, sort_by)  
@@ -30,5 +30,5 @@ if (isset($data['email'])) { //Email already exists
     $stmt->bindParam(':sort_by', $new);
     $stmt->execute();
     createMessage(2, 'Account has been created');
-    redirect('/../views/login.php');
+    redirect('views/login.php');
 }
