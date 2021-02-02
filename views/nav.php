1ed73362aad66e36b85c8c802d90773ae1f768af
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-02-02 23:05:09 +0100

    Removed nav option for page you are already on

--- views/nav.php
+++ views/nav.php
@@ -5,20 +5,25 @@
     <div class="hamburger-icon hamburger center-contain"></div>
     <div class="hamburger-menu hamburger">
         <div class="hamburger_text_container">
-            <a href="/index.php">
-                <h1 class="most-right-nav">Start</h1>
-            </a>
+            <?php $url = $_SERVER['REQUEST_URI']; ?>
+            <?php if ($url !== '/index.php') : ?>
+                <a href="/index.php">
+                    <h1 class="most-right-nav">Start</h1>
+                </a>
+            <?php endif ?>
             <?php
-            if (!isset($_SESSION['user'])) : ?>
+            if (!isset($_SESSION['user']) && $url !== '/views/login.php') : ?>
                 <a href="/views/login.php">
                     <h1>Login/Create account</h1>
                 </a>
             <?php endif ?>
-            <a href="/views/create_post.php">
-                <h1>Create post</h1>
-            </a>
+            <?php if ($url !== '/views/create_post.php') : ?>
+                <a href="/views/create_post.php">
+                    <h1>Create post</h1>
+                </a>
+            <?php endif ?>
             <?php
-            if (isset($_SESSION['user'])) : ?>
+            if (isset($_SESSION['user']) && $url !== "/views/account.php") : ?>
                 <a href="/views/account.php">
                     <h1>Account</h1>
                 </a>
@@ -26,7 +31,7 @@
             <?php
             if (isset($_SESSION['user'])) : ?>
                 <a href="/account/logout.php">
-                    <h1>Logout</h1>
+                    <h1 class="red-shadow">Logout</h1>
                 </a>
             <?php endif ?>
         </div>
